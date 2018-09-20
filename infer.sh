84729d48c26b3040b1cba3959252c97530da7c3d
CodeWinger <mathieu.boucher5@gmail.com>
2018-09-20 15:33:23 -0400

    Using version from github

--- infer.sh
+++ infer.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-set -ev
-
-# Install Opaml
-wget https://github.com/ocaml/opam/releases/download/1.2.2/opam-1.2.2-x86_64-Linux -O opam
-chmod ugo+x opam
-./opam init --yes --comp=4.01.0 #(then say 'y' to the final question)
-eval `./opam config env`
-./opam install --yes sawja.1.5 atdgen.1.5.0 javalib.2.3a extlib.1.5.4 #(then say 'y' to the question)
-
-# Install infer (latest version)
+# Checkout Infer
 git clone https://github.com/facebook/infer.git
-#cd infer/
-make -C infer java
+cd infer
+# Compile Infer
+./build-infer.sh java
+# install Infer system-wide...
+sudo make install
+# ...or, alternatively, install Infer into your PATH
 export PATH=`pwd`/infer/bin:$PATH
-cd ../
