c365535a7d7e800cbdd76fc44be1f6cba09b05e2
CodeWinger <mathieu.boucher5@gmail.com>
2018-09-21 14:06:34 -0400

    SonarCloud Integration

--- src/main/java/mcgill/ecse429/tutorial2/App.java
+++ src/main/java/mcgill/ecse429/tutorial2/App.java
@@ -8,13 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        int x = -1;
-        System.out.println("ntesting sonar qube");
-    }
-    
-    int test() {
-        String s = null;
-        return s.length();
+    	if (args == null || args.length == 0) {
+    		System.out.println("no input");
+    		return;
+    	} 
+    	
+    	for (String argument : args) {
+    		System.out.println(argument);
+    	}
     }
 }
--- src/test/java/mcgill/ecse429/tutorial2/AppTest.java
+++ src/test/java/mcgill/ecse429/tutorial2/AppTest.java
@@ -8,7 +8,18 @@ import org.junit.Test;
 public class AppTest 
 {
 	@Test
-	public void testMainMethodForFullCoverage() {
+	public void testMainMethodForNull() {
 		App.main(null);
+	}
+	
+	@Test
+	public void testMainMethodForEmptyArray() {
+		App.main(new String[0]);
+	}
+	
+	@Test
+	public void testMainMethodForNonEmptyArray() {
+		String[] args = {"Hello", "World"};
+		App.main(args);
 	}
 }
